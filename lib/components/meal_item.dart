eef3700cfd40d9a2c720bb707a9bdbfa2de96fcc
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-05-06 12:12:47 -0300

    MealItem, Getters Meals

---
+++ lib/components/meal_item.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem(this.meal, {Key? key}) : super(key: key);
+  final Meal meal;
+
+  _selectMeal() {}
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Theme.of(context).colorScheme.primary,
+      onTap: () => _selectMeal(),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: <Widget>[
+                      const Icon(Icons.schedule),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text('${meal.duration} minutos'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(width: 6),
+                      Text(meal.complexityText)
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 6),
+                      Text(meal.costText)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import '../components/meal_item.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final Category category =
-        ModalRoute.of(context)?.settings.arguments as Category;
+    final category = ModalRoute.of(context)?.settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Receitas por categoria ${category.title}'),
-          ],
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (context, index) {
+            return MealItem(categoryMeals[index]);
+          },
         ),
       ),
     );
